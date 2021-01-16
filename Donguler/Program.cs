7efe3f2194deb3dafb90fd2ad0fb375081c34fcf
c1411019 <c1411019@student.cankaya.edu.tr>
2021-01-16 23:42:52 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array tanımlama
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç İçin Temel Kurs", "Java Kursu" };
+
+            for(int i=0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("********************");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("********************");
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine("Ürünün adı: " + urun.Adi);
+                Console.WriteLine("Ürünün fiyatı: " + urun.Fiyati + " TL");
+                Console.WriteLine("Ürünün açıklaması: " + urun.Aciklama);
+                Console.WriteLine("---------------------------");
+            }
+
+            Console.WriteLine("***************Metotlar***************");
+            
+            //instance -- örnek
+            //Encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+
+            Console.ReadLine();
+        }
+    }
+}
